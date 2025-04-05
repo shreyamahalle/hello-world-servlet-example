8dff9d4064dec80431c0e80f9cd383b2daebc33e
shreya mahalle <shreyamahalle2000@gmail.com>
2025-04-05 14:48:18 +0530

    servlet working example added

--- src/main/java/com/shreya/servlet/controller/CustomerController.java
+++ src/main/java/com/shreya/servlet/controller/CustomerController.java
@@ -1,4 +1,39 @@
 package com.shreya.servlet.controller;
 
-public class CustomerController {
+import com.shreya.servlet.service.CustomerService;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+public class CustomerController extends HttpServlet {
+    private final CustomerService customerService = new CustomerService();
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        System.out.println("--------------- inside the doGet() method ---------------");
+
+        String name = request.getParameter("name");
+        response.setContentType("text/html");
+        PrintWriter writer = response.getWriter();
+        writer.println("<html><body>");
+        writer.println("<h1>" + customerService.greet(name) + "</h1>");
+        writer.println("</body></html>");
+    }
+
+    @Override
+    public void service(HttpServletRequest request, HttpServletResponse response) throws  IOException, ServletException {
+        System.out.println("--------------- inside the service() method ---------------");
+        super.service(request,response);
+    }
+
+    @Override
+    public void destroy() {
+        System.out.println("--------------- inside the destroy() method ---------------");
+    }
 }
+
+
--- src/main/java/com/shreya/servlet/service/CustomerService.java
+++ src/main/java/com/shreya/servlet/service/CustomerService.java
@@ -1,4 +1,11 @@
 package com.shreya.servlet.service;
 
 public class CustomerService {
+
+    public String greet(String name) {
+    if(name.isEmpty()){
+        return  "hello all";
+    }
+    return "hello " + name;
+}
 }
